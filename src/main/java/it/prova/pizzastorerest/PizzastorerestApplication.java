14fd23a091d49730f07a34bcd1cab2cd7fbb0b9a
RobertoEsposito02 <robiespo02@libero.it>
2022-12-24 12:57:33 +0100

    creazione progetto

---
+++ src/main/java/it/prova/pizzastorerest/PizzastorerestApplication.java
@@ -0,0 +1,13 @@
+package it.prova.pizzastorerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzastorerestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzastorerestApplication.class, args);
+	}
+
+}
