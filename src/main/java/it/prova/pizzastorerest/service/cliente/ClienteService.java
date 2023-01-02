0d8413e93b32ee0e34710830dd651b01eb25b285
RobertoEsposito02 <robiespo02@libero.it>
2023-01-02 17:50:56 +0100

    creati service per cliente

---
+++ src/main/java/it/prova/pizzastorerest/service/cliente/ClienteService.java
@@ -0,0 +1,18 @@
+package it.prova.pizzastorerest.service.cliente;
+
+import java.util.List;
+
+import it.prova.pizzastorerest.model.Cliente;
+
+public interface ClienteService {
+	public List<Cliente> listAll();
+
+	public Cliente caricaSingoloElemento(Long id);
+
+	public void aggiorna(Cliente clienteInstance);
+
+	public void inserisciNuovo(Cliente clienteInstance);
+
+	public void rimuovi(Long idToRemove);
+
+}
---
+++ src/main/java/it/prova/pizzastorerest/service/cliente/ClienteServiceImpl.java
@@ -0,0 +1,40 @@
+package it.prova.pizzastorerest.service.cliente;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import it.prova.pizzastorerest.model.Cliente;
+import it.prova.pizzastorerest.repository.cliente.ClienteRepository;
+
+public class ClienteServiceImpl implements ClienteService{
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Override
+	public List<Cliente> listAll() {
+		return (List<Cliente>) clienteRepository.findAll();
+	}
+
+	@Override
+	public Cliente caricaSingoloElemento(Long id) {
+		return clienteRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public void aggiorna(Cliente clienteInstance) {
+		clienteRepository.save(clienteInstance);
+	}
+
+	@Override
+	public void inserisciNuovo(Cliente clienteInstance) {
+		clienteRepository.save(clienteInstance);
+	}
+
+	@Override
+	public void rimuovi(Long idToRemove) {
+		clienteRepository.deleteById(idToRemove);
+	}
+
+}
