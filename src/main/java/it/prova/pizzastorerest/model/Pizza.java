086b2f2ad389407131c72d6bbcae8cf8ceff0719
RobertoEsposito02 <robiespo02@libero.it>
2023-01-02 17:14:49 +0100

    creata classe model pizza

---
+++ src/main/java/it/prova/pizzastorerest/model/Pizza.java
@@ -0,0 +1,34 @@
+package it.prova.pizzastorerest.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Entity
+@Table(name = "pizza")
+public class Pizza {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private long id;
+	@Column(name = "descrizione")
+	private String descrizione;
+	@Column(name = "ingredientti")
+	private String[] ingredienti;
+	@Column(name = "prezzobase")
+	private Integer prezzoBase;
+	@Column(name = "attivo")
+	private boolean attivo;
+}
