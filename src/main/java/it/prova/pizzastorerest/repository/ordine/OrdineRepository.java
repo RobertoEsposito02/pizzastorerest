64812b33ccd82bb5f7e79c39eeba858c7d1b17fb
RobertoEsposito02 <robiespo02@libero.it>
2023-01-03 17:34:05 +0100

    buf-fix metodo findByIdConPizze per ordine

--- src/main/java/it/prova/pizzastorerest/repository/ordine/OrdineRepository.java
+++ src/main/java/it/prova/pizzastorerest/repository/ordine/OrdineRepository.java
@@ -9,6 +9,6 @@ import it.prova.pizzastorerest.model.Ordine;
 
 public interface OrdineRepository extends CrudRepository<Ordine, Long>, CustomOrdineRepository{
 	
-	@Query("from Ordine o left join fetch where o.id = :id")
+	@Query("from Ordine o left join fetch o.pizze p where o.id = :id")
 	Optional<Ordine> findByIdConPizze(Long id);
 }
