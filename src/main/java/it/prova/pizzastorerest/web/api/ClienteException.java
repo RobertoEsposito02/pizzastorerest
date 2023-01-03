19ab61ec79362ba0bf6e9ce30bb2629315c154b6
RobertoEsposito02 <robiespo02@libero.it>
2023-01-03 13:55:30 +0100

    implementato controller per clinete

---
+++ src/main/java/it/prova/pizzastorerest/web/api/ClienteException.java
@@ -0,0 +1,82 @@
+package it.prova.pizzastorerest.web.api;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import it.prova.pizzastorerest.dto.cliente.ClienteDTO;
+import it.prova.pizzastorerest.model.Cliente;
+import it.prova.pizzastorerest.service.cliente.ClienteService;
+import it.prova.pizzastorerest.web.api.exception.ClienteNotFoundException;
+import it.prova.pizzastorerest.web.api.exception.IdNotNullForInsertException;
+import it.prova.pizzastorerest.web.api.exception.IdNullForUpdateException;
+
+@RestController
+@RequestMapping("/api/cliente")
+public class ClienteException {
+	
+	@Autowired
+	private ClienteService clienteService;
+	
+	public List<ClienteDTO> listAll(){
+		return ClienteDTO.buildDTOListFromModel(clienteService.listAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ClienteDTO findById(@PathVariable(required = true) Long id) {
+		Cliente clienteDaCaricare = clienteService.caricaSingoloElemento(id);
+		if(clienteDaCaricare == null)
+			throw new ClienteNotFoundException("cliente non trovato");
+		
+		return ClienteDTO.buildDTOFromModel(clienteDaCaricare);
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ClienteDTO insert(@Valid @RequestBody ClienteDTO cliente) {
+		if(cliente.getId() != null)
+			throw new IdNotNullForInsertException("impossibile aggiungere un record se il campo id e' stato valorizzato");
+		
+		clienteService.inserisciNuovo(cliente.buildModelFromDto());
+		
+		return cliente;
+	}
+	
+	@PutMapping
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void update(@Valid @RequestBody ClienteDTO clienteDTO) {
+		if(clienteDTO.getId() == null)
+			throw new IdNullForUpdateException("impossibile aggiornare un record se non si valorizza il campo id");
+		
+		Cliente clienteDaAggiornare = clienteService.caricaSingoloElemento(clienteDTO.getId());
+		if(clienteDaAggiornare == null)
+			throw new ClienteNotFoundException("cliente non trovato");
+		
+		clienteService.aggiorna(clienteDTO.buildModelFromDto());
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable(required = true) Long id) {
+		if(id == null)
+			throw new ClienteNotFoundException("cliente non trovato");
+		
+		Cliente clienteDaEliminare = clienteService.caricaSingoloElemento(id);
+		if(clienteDaEliminare == null)
+			throw new ClienteNotFoundException("cliente non trovato");
+		
+		clienteService.rimuovi(id);
+	}
+}
