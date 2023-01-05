de2a0b31e120d37aa593f1d0495613bf15a0ec9d
RobertoEsposito02 <robiespo02@libero.it>
2023-01-05 16:34:19 +0100

    aggiustato nome della classe, aggiunto annotation getmmapping sul listall, e crossorigin

--- src/main/java/it/prova/pizzastorerest/web/api/ClienteController.java
+++ src/main/java/it/prova/pizzastorerest/web/api/ClienteController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,11 +26,13 @@ import it.prova.pizzastorerest.web.api.exception.IdNullForUpdateException;
 
 @RestController
 @RequestMapping("/api/cliente")
-public class ClienteException {
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
+public class ClienteController {
 	
 	@Autowired
 	private ClienteService clienteService;
 	
+	@GetMapping
 	public List<ClienteDTO> listAll(){
 		return ClienteDTO.buildDTOListFromModel(clienteService.listAll());
 	}
